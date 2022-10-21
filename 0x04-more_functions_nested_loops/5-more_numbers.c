4829c6b1920b7e9f699f16f2c59c2ec10ffd07ce
agurey34 <bradleyagurey34@gmail.com>
2022-10-21 13:20:53 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,9 @@ void more_numbers(void)
 		{
 			if (num > 9)
 			{
-				_putchar((num / 10) = '0')
+				_putchar((num / 10) = '0');
 			}
-			_putchar((num % 10) + '0')
+			_putchar((num % 10) + '0');
 		}
 
 		-putchar('\n');
